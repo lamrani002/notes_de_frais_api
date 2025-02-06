27a4c39f8e53852c05200450b3bcd98ccdad8881
Lamrani Abdel <lamrani002@gmail.com>
2025-02-06 23:59:51 +0100

    Creation des routes de l api

--- app/Http/Requests/ExpenseNoteRequest.php
+++ app/Http/Requests/ExpenseNoteRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ExpenseNoteRequest extends FormRequest
 {
@@ -49,4 +51,19 @@ class ExpenseNoteRequest extends FormRequest
             'company_id.exists' => 'L\'entreprise sélectionnée n\'existe pas.',
         ];
     }
+
+
+    /**
+     * Personnalise la réponse des erreurs de validation pour éviter les doublons.
+     *
+     * @param Validator $validator
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors()->first(),
+        ], 422));
+    }
+
 }
